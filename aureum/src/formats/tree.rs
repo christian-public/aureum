74e67d114854177400131ef7253f8e4f3a7705b5
Christian Vårvik <christian@christian.pub>
2026-04-02 15:58:36 +0200

    Dim the borders and line numbers when showing test failures

--- aureum/src/formats/tree.rs
+++ aureum/src/formats/tree.rs
@@ -1,5 +1,6 @@
 use crate::test_result::{TestResult, ValueComparison};
 use crate::utils::string;
+use crate::utils::string::TextBlockOptions;
 use crate::vendor::ascii_tree;
 use crate::vendor::ascii_tree::Tree::{self, Leaf, Node};
 use colored::Colorize;
@@ -46,66 +47,85 @@ fn format_string_diff(expected: &str, got: &str) -> Vec<Tree> {
         .to_string()
         .len();
     let blank = " ".repeat(width);
+    let separator = "│".dimmed();
 
     let format_expected_line = |num: usize, line: &str| -> String {
-        let num_str = format!("{num:>width$}");
+        let num_str = format!("{num:>width$}").dimmed();
         if line.is_empty() {
-            format!("{num_str} {blank} │")
+            format!("{num_str} {blank} {separator}")
         } else {
-            format!("{num_str} {blank} │  {line}")
+            format!("{num_str} {blank} {separator}  {line}")
         }
     };
 
     let format_got_line = |num: usize, line: &str| -> String {
-        let num_str = format!("{num:>width$}");
+        let num_str = format!("{num:>width$}").dimmed();
         if line.is_empty() {
-            format!("{blank} {num_str} │")
+            format!("{blank} {num_str} {separator}")
         } else {
-            format!("{blank} {num_str} │  {line}")
+            format!("{blank} {num_str} {separator}  {line}")
         }
     };
 
     let format_diff_line = |left_num: Option<usize>, right_num: Option<usize>, line: &str| {
-        let left_num_str = left_num.map_or(blank.clone(), |num| format!("{num:>width$}"));
-        let right_num_str = right_num.map_or(blank.clone(), |num| format!("{num:>width$}"));
+        let left_num_str = left_num
+            .map_or(blank.clone(), |num| format!("{num:>width$}"))
+            .dimmed();
+        let right_num_str = right_num
+            .map_or(blank.clone(), |num| format!("{num:>width$}"))
+            .dimmed();
         match (left_num, right_num) {
             (Some(_), None) => {
-                let text = format!("-{}", line).red().to_string();
-                format!("{left_num_str} {blank} │ {text}")
+                let text = format!("-{}", line).red();
+                format!("{left_num_str} {blank} {separator} {text}")
             }
             (None, Some(_)) => {
-                let text = format!("+{}", line).green().to_string();
-                format!("{blank} {right_num_str} │ {text}")
+                let text = format!("+{}", line).green();
+                format!("{blank} {right_num_str} {separator} {text}")
             }
             _ => {
                 if line.is_empty() {
-                    format!("{left_num_str} {right_num_str} │")
+                    format!("{left_num_str} {right_num_str} {separator}")
                 } else {
-                    format!("{left_num_str} {right_num_str} │  {line}")
+                    format!("{left_num_str} {right_num_str} {separator}  {line}")
                 }
             }
         }
     };
 
+    let text_block_options = TextBlockOptions {
+        top_line: TextBlockOptions::CORNER_TOP.dimmed().to_string(),
+        bottom_line: TextBlockOptions::CORNER_BOTTOM.dimmed().to_string(),
+        format_line: |line| {
+            if line.is_empty() {
+                TextBlockOptions::BORDER.dimmed().to_string()
+            } else {
+                format!("{} {line}", TextBlockOptions::BORDER.dimmed())
+            }
+        },
+    };
+
     let expected_lines = string_to_lines(&format!(
         "Expected\n{}",
-        string::text_block(&string::prefix_text_with_line_numbers(
-            expected,
-            format_expected_line,
-        ))
+        string::text_block_with_options(
+            &string::prefix_text_with_line_numbers(expected, format_expected_line,),
+            &text_block_options,
+        )
     ));
     let got_lines = string_to_lines(&format!(
         "Got\n{}",
-        string::text_block(&string::prefix_text_with_line_numbers(got, format_got_line)),
+        string::text_block_with_options(
+            &string::prefix_text_with_line_numbers(got, format_got_line),
+            &text_block_options,
+        ),
     ));
 
     let diff_lines = string_to_lines(&format!(
         "Diff\n{}",
-        string::text_block(&string::prefix_diff_with_line_numbers(
-            expected,
-            got,
-            format_diff_line,
-        ))
+        string::text_block_with_options(
+            &string::prefix_diff_with_line_numbers(expected, got, format_diff_line,),
+            &text_block_options,
+        )
     ));
 
     vec![Leaf(expected_lines), Leaf(got_lines), Leaf(diff_lines)]
